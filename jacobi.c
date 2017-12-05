47456085588f31cb11a78931d84c4e4068b204ce
maiv <maiv@wwu.edu>
2017-12-04 16:41:52 -0800

    Inital structure, starting testing with 2 threads

--- jacobi.c
+++ jacobi.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 void* helloThread();
-void readInValues(double (*mtx)[1024]);
+void readInValues(double (*mtx)[4]);
 
 //Lab computers have 8 threads total
 int main (int argc, const char* argv[]){
@@ -14,50 +14,50 @@ int main (int argc, const char* argv[]){
     */
 
     //Make space for the arrays
-    double (* mtx) [1024] = malloc (1024* 1024* sizeof (double));
-    double (* tmp) [1024] = malloc (1024* 1024* sizeof (double));
-
-    FILE* inputFile = fopen("./input.mtx", "r");
-
-    if(inputFile == NULL){
-        fprintf(stderr, "Opening input file failed\n");
-        exit(1);
-    }
-
-    for (int i = 0; i < 1024; i++){
-        for (int j = 0; j < 1024; j++){
-            fscanf(inputFile, "%lf", &mtx[i][j]);
-        }
-    }
-    fclose(inputFile);
+    double (* mtx) [4] = malloc (4* 4* sizeof (double));
+    double (* tmp) [4] = malloc (4* 4* sizeof (double));
 
 
-    //readInValues(mtx);
+    readInValues(mtx);
 
 
     pthread_t thread1, thread2;
     const char *message1 = "Thread 1";
     const char *message2 = "Thread 2";
-    int  iret1, iret2;
+    //Array that contains arguments
+    
 
-    iret1 = pthread_create(&thread1, NULL, helloThread, &num1);
+    //Pass a number associated with each thread
+    int num1 = 1;
+    int num2 = 2;
+
+    //Could make a flag that is changed when any of the threads find that the difference is too high
+    //As each thread is filling in numbers they should be checking the difference between the current number they are adding and the same position on the old matrix
+
+
+    num1 = pthread_create(&thread1, NULL, helloThread, &num1);
     if(iret1){
-        fprintf(stderr, "Error - pthread_create() return code: %d\n", iret1);
+        fprintf(stderr, "Error - pthread_create() return code: %d\n", num1);
         exit(EXIT_FAILURE);
     }
 
-    iret2 = pthread_create( &thread2, NULL, helloThread, &num2);
+    num2 = pthread_create( &thread2, NULL, helloThread, &num2);
     if(iret2)
     {
-        fprintf(stderr,"Error - pthread_create() return code: %d\n",iret2);
+        fprintf(stderr,"Error - pthread_create() return code: %d\n", num2);
         exit(EXIT_FAILURE);
     }
 
-    dprintf(2, "pthread_create() for thread 1 returns: %d\n",iret1);
-    dprintf(2, "pthread_create() for thread 2 returns: %d\n",iret2);
+    dprintf(2, "pthread_create() for thread 1 returns: %d\n", num1);
+    dprintf(2, "pthread_create() for thread 2 returns: %d\n", num2);
 
+
+    //Join threads together after work is done
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
+
+    //Check iteration flag
+
     exit(EXIT_SUCCESS);
     //read in the input matrix
     //Will have to decide on how to handle threads
@@ -70,11 +70,13 @@ int main (int argc, const char* argv[]){
 Ideas:
 Calc by row
 1.) start each thread at their row number and increment by number of threads
-2.) 
+2.)
 
 jacobi algorithm (sub new and old for input and output arrays)
-for (int i = 1; i < 1023; i++){
-    for (int j = 1; j < 1023; j++){
+
+
+for (i = 1; i < 1023; i++){
+    for (j = 1; j < 1023; j++){
         new[i][j] = (old[i-1][j] +
         old[i+1][j] +
         old[i][j-1] +
@@ -83,13 +85,26 @@ for (int i = 1; i < 1023; i++){
 }
 */
 
-void* helloThread(int* num){
+void* jacobi(int* noth, int* numT){
     printf("Hello other thread %d\n", *num);
 }
 
-void readInValues(double (*mtx)[1024]){
+void readInValues(double (*mtx)[4]){
+        FILE* inputFile = fopen("./testInput", "r");
 
+        if(inputFile == NULL){
+            fprintf(stderr, "Opening input file failed\n");
+            exit(1);
+        }
 
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
+                fscanf(inputFile, "%lf", &mtx[i][j]);
+            }
+        }
+        printf("first: %lf\n", mtx[0][0]);
+        printf("last: %lf\n", mtx[3][3]);
+        fclose(inputFile);
 }
 
 
