0e5a2ba6e9c9110ce0134e753a03daa63735303b
carrolp2 <carrolp2@wwu.edu>
2017-12-06 18:22:39 -0800

    Threads kind of working, need more testing. Working on barriers

--- jacobi.c
+++ jacobi.c
@@ -1,16 +1,19 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+
+#define SIZE 4
+#define NOTH 2
+#define DELTA .1
 
 void* jacobi(void* ptr);
-void readInValues(double (*mtx)[4]);
-
-#define NOTH 1
+void readInValues(double (*mtx)[SIZE]);
 
 typedef
 struct arg_st{
-    double (* mtxLEFT) [4];
-    double (* mtxRIGHT) [4];
+    double (* mtxLEFT) [SIZE];
+    double (* mtxRIGHT) [SIZE];
     int n;
     int t;
     int j;
@@ -21,16 +24,12 @@ struct arg_st{
 int main (int argc, const char* argv[]){
 
 /* WORKNOTE:
- * try to parse arguments from void*
- * figure out how to stop and swap matrix that we are looking at
- * increment for loops based on thread number
- */
 
     /*
     *Read in array, might need to move this to another function
     */
-    double (* mtxL) [4] = malloc (4* 4* sizeof (double));
-    double (* mtxR) [4] = malloc (4* 4* sizeof (double));
+    double (* mtxL) [SIZE] = malloc (SIZE* SIZE* sizeof (double));
+    double (* mtxR) [SIZE] = malloc (SIZE* SIZE* sizeof (double));
     pthread_t thd[NOTH];
     arg_t args[NOTH];
     int j;
@@ -38,10 +37,11 @@ int main (int argc, const char* argv[]){
     readInValues(mtxL);
     readInValues(mtxR);
 
+
     for(j = 0; j < NOTH; j++){
         args[j].mtxLEFT = mtxL;
         args[j].mtxRIGHT = mtxR;
-        args[j].n = 4;
+        args[j].n = SIZE;
         args[j].t = NOTH;
         args[j].j = j;
         if(pthread_create(&thd[j], NULL, &jacobi, &args[j])){
@@ -75,22 +75,52 @@ void* jacobi(void* ptr){
     int i;
     int j;
     int from = (args->j*args->n)/args->t;
-    int to = ((args->j+1)*args->n)/args->t;
+    int to = ((args->j+1)*args->n)/args->t - args->j;
+    if (from == 0)
+        from = 1;
+    if (to == args->n)
+        to = args->n - 1;
+    double max;
+    double test;
+    int done = 0;
 
-    for(i = from; i < to; i++){
-        for(j = 0; j < 3; j++){
-            args->mtxLEFT[i][j] = (args->mtxRIGHT[i-1][j] +
-                        args->mtxRIGHT[i+1][j] +
-                        args->mtxRIGHT[i][j-1] +
-                        args->mtxRIGHT[i][j+1]) / 4.0;
-            printf("mtxLEFT[%d][%d]: %lf\n", i, j , args->mtxLEFT[i][j]);
+    printf("from: %d thread: %d\n" , from, args->j);
+    printf("to: %d thread: %d\n" , to - 1, args->j);
+
+    while (!done){
+        max = 0;
+
+        for(int i = from; i < to; i++){
+            for(int j = 1; j < SIZE-1; j++){
+                double prev = args->mtxLEFT[i][j];
+                args->mtxLEFT[i][j] = (args->mtxRIGHT[i-1][j] +
+                            args->mtxRIGHT[i+1][j] +
+                            args->mtxRIGHT[i][j-1] +
+                            args->mtxRIGHT[i][j+1]) / 4.0;
+                printf("mtxLEFT[%d][%d]: %.2lf\n", i, j , args->mtxLEFT[i][j]);
+                test = args->mtxLEFT[i][j] - prev;
+                if (test > max)
+                    max = test;
+
+                //printf("max: %lf\n", max);
+            }
         }
+
+        if (max < DELTA)
+            done = 1;
+        else {
+            double (* temp)[SIZE] = args->mtxLEFT;
+            args->mtxLEFT = args->mtxRIGHT;
+            args->mtxRIGHT = temp;
+        }
+
+
     }
 
-
+    return NULL;
 }
 
-void readInValues(double (*mtx)[4]){
+void readInValues(double (*mtx)[SIZE]){
         FILE* inputFile = fopen("./testInput", "r");
 
         if(inputFile == NULL){
@@ -98,18 +128,63 @@ void readInValues(double (*mtx)[4]){
             exit(1);
         }
 
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
+        for (int i = 0; i <= SIZE-1; i++){
+            for (int j = 0; j <= SIZE-1; j++){
                 fscanf(inputFile, "%lf", &mtx[i][j]);
             }
         }
-        printf("first: %lf\n", mtx[0][0]);
-        printf("last: %lf\n", mtx[3][3]);
+
+        for (int i = 0; i <= SIZE-1; i++){
+            printf("row %d: ", i);
+            for (int j = 0; j <= SIZE-1; j++){
+                printf("%0.lf ", mtx[i][j]);
+            }
+            printf("\n");
+        }
+
+
         fclose(inputFile);
 }
 
+typedef struct barrier_st{
+    int noth;
+    pthread_cond_t ready;
+    pthread_cond_t full;
+    pthread_mutex_t mtx;
+    int waiting;
+    int leaving;
+} barrier;
 
+barrier* barrier_new (int noth){
+    barrier* b = malloc(sizeof(barrier));
+    b->noth = noth;
 
+    return b;
+}
+
+void barrier_enter(barrier* b){
+    assert(b != NULL);
+    pthread_mutex_lock(&(b->mtx));
+    while (b->leaving > 0)
+        pthread_cond_wait(&(b->ready), &(b->mtx));
+        b->waiting++;
+
+    if(b->waiting != b->noth){
+        pthread_cond_wait(&(b->full), &(b->mtx));
+        b->leaving--;;
+    }
+    else{
+        b->leaving = b->waiting;
+        b->waiting = 0;
+        b->leaving--;
+        pthread_cond_broadcast(&(b->full));
+    }
+
+    if(b->leaving == 0)
+        pthread_cond_broadcast(&(b->ready));
+    pthread_mutex_unlock(&(b->mtx));
+
+}
 
 
 
